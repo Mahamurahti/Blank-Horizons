6fea0bccbb6db97592131de94102cf26b210ee03
Eric <55787260+Mahamurahti@users.noreply.github.com>
2021-09-25 16:09:19 +0300

    Logging in and registering preliminary work

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/Link'
 import styles from '../styles/Login.module.css'
 
+import { useState } from 'react'
+
 export default function Login() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(username + " " + password)
+    }
+
+    const printUser = () => console.log(username + " " + password)
+
     return (
         <div className={styles.container}>
             <Head>
@@ -16,9 +29,35 @@ export default function Login() {
                     Login
                 </h1>
 
-                <div className={styles.loginContainer}>
-                    This is the login container...
-                </div>
+                <form className={styles.form} onChange={printUser} onSubmit={handleSubmit}>
+                    <div className={styles.section}>
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            placeholder="Enter Username"
+                            name="username"
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                            required />
+                    </div>
+
+                    <div className={styles.section}>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            name="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            required />
+                    </div>
+
+                    <div className={styles.section}>
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+
+                <p>Not already a user? Register <Link href="/register"><a>here</a></Link>.</p>
             </main>
 
             <footer className={styles.footer}>
--- pages/register.js
+++ pages/register.js
@@ -2,7 +2,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Register.module.css'
 
+import { useState } from 'react'
+
 export default function Register() {
+    const [username, setUsername] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [password, setPassword] = useState("")
+    const [terms, setTerms] = useState(false)
+    const [country, setCountry] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log("%s %s %s %s %s %s", username, firstName, lastName, password, country, terms)
+    }
+
     return (
         <div className={styles.container}>
             <Head>
@@ -16,9 +30,84 @@ export default function Register() {
                     Register
                 </h1>
 
-                <div className={styles.leaderboardContainer}>
-                    This is the register container...
-                </div>
+                <form className={styles.form} onSubmit={handleSubmit}>
+                    <div className={styles.section}>
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            placeholder="Enter Username"
+                            name="username"
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                            required />
+                    </div>
+
+                    <div className={styles.section}>
+                        <label htmlFor="firstName">First Name</label>
+                        <input
+                            type="text"
+                            placeholder="Enter First Name"
+                            name="firstName"
+                            value={firstName}
+                            onChange={e => setFirstName(e.target.value)}
+                            required />
+                    </div>
+
+                    <div className={styles.section}>
+                        <label htmlFor="lastName">Last Name</label>
+                        <input
+                            type="text"
+                            placeholder="Enter Last Name"
+                            name="lastName"
+                            value={lastName}
+                            onChange={e => setLastName(e.target.value)}
+                            required />
+                    </div>
+
+                    <div className={styles.section}>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            name="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            required />
+                    </div>
+
+                    <div className={styles.section}>
+                        <label htmlFor="country">Country</label>
+                        <input
+                            type="text"
+                            placeholder="Enter Country"
+                            name="country"
+                            value={country}
+                            onChange={e => setCountry(e.target.value)}
+                            required />
+                    </div>
+
+                    <div className={styles.section + " " + styles.terms}>
+                        <label htmlFor="terms">
+                            Do you accept our {" "}
+                            <a
+                                href="https://www.youtube.com/watch?v=eBGIQ7ZuuiU"
+                                target="_blank"
+                            >
+                                terms and conditions
+                            </a>?
+                        </label>
+                        <input
+                            type="checkbox"
+                            name="terms"
+                            value={terms}
+                            onChange={e => setTerms(!terms)}
+                            required />
+                    </div>
+
+                    <div className={styles.section}>
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
             </main>
 
             <footer className={styles.footer}>
